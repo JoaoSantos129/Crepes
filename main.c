8dc2531acb8b3a4d7a2ccc0db63cc2ecf60959fd
JoaoSantos129 <Joao-Alexandre.CARVALHO-SANTOS@cpnv.ch>
2022-02-02 16:56:55 +0100

    Crpes exercice
    jai pas fini

--- main.c
+++ main.c
@@ -1,6 +1,35 @@
 #include <stdio.h>
 
+#define FLOUR_PER_PERSON 125
+#define MILK_PER_PERSON 0.25
+#define EGG_PER_PERSON 1.5
+#define SALT_PER_PERSON 0.5
+#define BUTTER_PER_PERSON 0.5
+
+int menu (){
+    int nbMangeurs;
+    printf("Combien de personnes mangent des crepes?");
+    scanf("%d", &nbMangeurs);
+
+    return nbMangeurs;
+}
+
+int calcul (nbPersons){
+    int farine = FLOUR_PER_PERSON * nbPersons;
+    int lait = MILK_PER_PERSON * nbPersons;
+    int oeufs = EGG_PER_PERSON * nbPersons;
+    int sel = SALT_PER_PERSON * nbPersons;
+    int beurre = BUTTER_PER_PERSON * nbPersons;
+
+    printf("\n\nLes ingredients necessaires:\n");
+    printf("\n\n - Farine : %d\n" );
+}
+
 int main() {
-    printf("Hello, World!\n");
+    int nbPersonnes = 0;
+
+    nbPersonnes = menu();
+    calcul(nbPersonnes);
+
     return 0;
 }
